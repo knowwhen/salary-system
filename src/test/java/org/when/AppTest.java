ca4074dd588d9eb4253b536b416dc03e162600ae
wen <knowhen@qq.com>
2023-03-30 17:22:24 +0800

    Init project for Salary System

---
+++ src/test/java/org/when/AppTest.java
@@ -0,0 +1,26 @@
+package org.when;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @org.junit.Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
