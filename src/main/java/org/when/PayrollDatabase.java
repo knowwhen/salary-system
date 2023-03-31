4ec111a1e3d639340fcd88000dbe3707fcdb49f0
wen <knowhen@qq.com>
2023-03-31 10:37:04 +0800

    Rename DataBase and make it singleton

---
+++ src/main/java/org/when/PayrollDatabase.java
@@ -0,0 +1,34 @@
+package org.when;
+
+import org.when.employee.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PayrollDatabase {
+    private static PayrollDatabase INSTANCE;
+
+    private PayrollDatabase() {
+    }
+
+    public static PayrollDatabase getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PayrollDatabase();
+        }
+        return INSTANCE;
+    }
+
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+    public Employee findEmployee(int employeeId) {
+        return employees.get(employeeId);
+    }
+
+    public void addEmployee(Integer employeeId, Employee employee) {
+        employees.put(employeeId, employee);
+    }
+
+    public void deleteEmployee(Integer employeeId) {
+        employees.remove(employeeId);
+    }
+}
--- src/test/java/org/when/DataBase.java
+++
@@ -1,9 +0,0 @@
-package org.when;
-
-import org.when.employee.Employee;
-
-public class DataBase {
-    public Employee findEmployee(int empId) {
-        return null;
-    }
-}
